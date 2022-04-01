7f804721a1c919fef76dceeeb743cb950e4fdc5c
JamFox <karl-andreas7@hotmail.com>
2022-04-01 22:01:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Spotify-ListLister
+
+Lists spotify user's playlists.
+
+Discovered a wonderful Spotify account that had over 6k playlists. Only 200 or so of them are displayed when looking at the profile through Spotify's website or app. Needed a way to list a list of all of their playlists so I can listen to the ones I'm interested in.
+
+## Installation
+
+1. Download the source.
+2. Create the configuration file:
+	- Rename listlister.conf.j2 to listlister.conf and fill in the fields.
+	OR 
+	- Create your own configuration file and supply the path to it with `-c` or `--cfg`.
+
+Auth configuration is required. Output is optional (that's why it's commented out), defaults to `playlists-<USER>.out`.
+```
+[auth]
+id = <App Client ID>
+secret = <App Client Secret>
+[output]
+#output_filename = <Path and name of file>
+```
+
+3. Install dependencies:
+	- `pip install spotify`
+	OR
+	- If you have trouble install packages using requirements.txt in a fresh venv.
+
+## Usage
+
+Run help: 
+`python listlister.py -h`
+
+Which displays:
+```
+listlister.py -h
+usage: listlister.py [-h] -u USER [-c CFG] [-o] [-f FILE]
+
+List users playlists and write them to file or terminal.
+
+required arguments:
+  -u USER, --user USER  User ID of whose playlists to list.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CFG, --cfg CFG     Path to configuration file. Defaults to listlister.conf.
+  -o, --out             Output to file instead of terminal if this flag is set.
+  -f FILE, --file FILE  Path to output file. Defaults to playlists-<USER>.out
+```
---
+++ listlister.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import asyncio
+import spotify
+import configparser
+import argparse
+from os.path import exists
+
+
+def configparse(configfile):
+    """Parse config"""
+    config = configparser.ConfigParser()
+    config.read(configfile)
+    return config
+
+
+def parse_arguments():
+    """Parse arguments"""
+    parser = argparse.ArgumentParser(
+        description="List users playlists and write them to file or terminal."
+    )
+    optional = parser._action_groups.pop() 
+    required = parser.add_argument_group('required arguments')
+    required.add_argument(
+        "-u",
+        "--user",
+        dest="user",
+        action="store",
+        help="User ID of whose playlists to list.",
+        type=str,
+        required=True,
+    )
+    optional.add_argument(
+        "-c",
+        "--cfg",
+        default="listlister.conf",
+        dest="cfg",
+        action="store",
+        help="Path to configuration file. Defaults to listlister.conf.",
+        type=str,
+    )
+    optional.add_argument(
+        "-o",
+        "--out",
+        action="store_true",
+        help="Output to file instead of terminal if this flag is set.",
+    )
+    optional.add_argument(
+        "-f",
+        "--file",
+        default=None,
+        dest="file",
+        action="store",
+        help="Path to output file. Defaults to playlists-<USER>.out",
+        type=str,
+    )
+    parser._action_groups.append(optional)
+    return parser.parse_args()
+
+
+async def get_user(client, user_id):
+    """Get spotify.User object"""
+    return await client.get_user(user_id)
+
+
+async def get_all_playlists(user):
+    """Get list of spotify.Playlist objects"""
+    return await user.get_all_playlists()
+
+
+def main():
+    args = parse_arguments()
+
+    if not exists(args.cfg):
+        raise Exception(
+            f"ERROR: configuration file at {args.cfg} does not exist.\n Create a configuration file from template listlister.conf.j2 by removing .j2 suffix OR use argument --cfg <path to config> to your custom path instead."
+        )
+    else:
+        config = configparse(args.cfg)
+
+    client = spotify.Client(config.get("auth", "id"), config.get("auth", "secret"))
+
+    loop = asyncio.get_event_loop()
+
+    user = loop.run_until_complete(get_user(client, args.user))
+
+    all_playlists = loop.run_until_complete(get_all_playlists(user))
+
+    if args.out:
+        for playlist in all_playlists:
+            print(playlist.name)
+    else:
+        default_output = "playlists-" + args.user + ".out"
+        if exists(default_output):
+            print(
+                f"A list of this user's playlists was already created. Filename: {default_output}"
+            )
+        else:
+            with open(
+                config.get("output", "output_filename", fallback=default_output),
+                "a",
+            ) as f:
+                for playlist in all_playlists:
+                    f.write(playlist.name + "\n")
+
+
+if __name__ == "__main__":
+    main()
